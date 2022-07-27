90f8aed72a3a0e98a72075a521ab07d606f8af95
Rakesh Kumar <100530475+iitp-rakesh@users.noreply.github.com>
2022-07-27 19:43:56 +0530

    Insertion Sort Algorithm Added

--- sort.c
+++ sort.c
@@ -1,30 +1,34 @@
-#include<stdio.h>
-//Bubble Sort Function
+#include <stdio.h>
+// Bubble Sort Function
 void bubbleSort(int arr[], int n)
 {
     int i, j, temp;
-    for (i = 0; i < n-1; i++)
+    for (i = 0; i < n - 1; i++)
     {
-        for (j = 0; j < n-i-1; j++)
+        for (j = 0; j < n - i - 1; j++)
         {
-            if (arr[j] > arr[j+1])
+            if (arr[j] > arr[j + 1])
             {
                 temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
-//Selection Sort Function
-void selectionSort(int arr[], int n){
-    int i, j, temp,index;
-    for (i = 0; i < n-1; i++){
-        int min= arr[i];
-        for (j = i; j < n; j++){
-            if (min>arr[j]){
-                min= arr[j];
-                index= j;
+// Selection Sort Function
+void selectionSort(int arr[], int n)
+{
+    int i, j, temp, index;
+    for (i = 0; i < n - 1; i++)
+    {
+        int min = arr[i];
+        for (j = i; j < n; j++)
+        {
+            if (min > arr[j])
+            {
+                min = arr[j];
+                index = j;
             }
         }
         temp = arr[index];
@@ -32,32 +36,54 @@ void selectionSort(int arr[], int n){
         arr[i] = temp;
     }
 }
-int main(){
+// Insertion sort Function
+void Insertionsort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+int main()
+{
     int size;
     printf("Enter the size of the array: ");
-    scanf("%d",&size);
+    scanf("%d", &size);
     int array[size];
     printf("Enter the all elements of the array: ");
-    for(int i=0;i<size;i++){
-        scanf("%d",&array[i]);
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &array[i]);
     }
     printf("Select the sorting algorithm:\n1. Bubble Sort\n2. Selection Sort\n3. Insertion Sort\n4. Merge Sort\n5. Quick Sort\nEnter your choice: ");
     int choice;
-    scanf("%d",&choice);
+    scanf("%d", &choice);
     switch (choice)
     {
     case 1:
-        bubbleSort(array,size);
+        bubbleSort(array, size);
         break;
     case 2:
-        selectionSort(array,size);
+        selectionSort(array, size);
+        break;
+    case 3:
+        Insertionsort(array, size);
         break;
     default:
         break;
     }
     printf("The sorted array is: ");
-    for(int i=0;i<size;i++){
-        printf("%d ",array[i]);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
     }
     return 0;
 }
