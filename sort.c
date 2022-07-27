e6f81d0f7f10c7a5d87842c613cd9a123c8817c7
Rakesh Kumar <100530475+iitp-rakesh@users.noreply.github.com>
2022-07-27 15:46:52 +0530

    Bubble sort algorith added

--- sort.c
+++ sort.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
-
+//Bubble Sort Function
+void bubbleSort(int arr[], int n)
+{
+    int i, j, temp;
+    for (i = 0; i < n-1; i++)
+    {
+        for (j = 0; j < n-i-1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
 int main(){
     int size;
     printf("Enter the size of the array: ");
@@ -9,16 +25,21 @@ int main(){
     for(int i=0;i<size;i++){
         scanf("%d",&array[i]);
     }
-    printf("Select the sorting algorithm:\n1. Bubble Sort\n2. Selection Sort\n3. Insertion Sort\n4. Merge Sort\n5. Quick Sort\n");
+    printf("Select the sorting algorithm:\n1. Bubble Sort\n2. Selection Sort\n3. Insertion Sort\n4. Merge Sort\n5. Quick Sort\nEnter your choice: ");
     int choice;
     scanf("%d",&choice);
     switch (choice)
     {
-    case 
+    case 1:
+        bubbleSort(array,size);
         break;
     
     default:
         break;
+    }
+    printf("The sorted array is: ");
+    for(int i=0;i<size;i++){
+        printf("%d ",array[i]);
     }
     return 0;
 }
