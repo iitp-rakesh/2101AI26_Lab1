c74c2ab4260be4801e9bfd84ddf0f97723872e19
Rakesh Kumar <100530475+iitp-rakesh@users.noreply.github.com>
2022-07-27 13:56:05 +0530

    Initial Commit

---
+++ calculator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float num1,num2,result;
+    char op;
+    printf("Enter the first number: ");
+    scanf("%f",&num1);
+    printf("Enter the second number: ");
+    scanf("%f",&num2);
+    printf("Enter the operator(+,-,*,/): ");
+    scanf(" %c",&op);
+    return 0;
+}
