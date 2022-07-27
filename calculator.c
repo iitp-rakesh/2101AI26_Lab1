292c0452c3b846a970d830c1a65def7b1173b4c4
Rakesh Kumar <100530475+iitp-rakesh@users.noreply.github.com>
2022-07-27 13:57:46 +0530

    Add Function added

--- calculator.c
+++ calculator.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
+// Add function
+float add(float a, float b)
+{
+    return a + b;
+}
+
 int main(){
     float num1,num2,result;
     char op;
@@ -8,5 +14,11 @@ int main(){
     scanf("%f",&num2);
     printf("Enter the operator(+,-,*,/): ");
     scanf(" %c",&op);
+    switch(op){
+        case '+':
+            result = add(num1,num2);
+            printf("%.2f + %.2f = %.2f\n",num1,num2,result);
+            break;
+    }
     return 0;
 }
