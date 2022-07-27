131d145ab6ce76967245da79395b50d3c8565146
Rakesh Kumar <100530475+iitp-rakesh@users.noreply.github.com>
2022-07-27 22:52:00 +0530

    Merge Sort Algorithm Added

--- sort.c
+++ sort.c
@@ -52,6 +52,85 @@ void Insertionsort(int arr[], int n)
         arr[j + 1] = key;
     }
 }
+// MergeSort Function
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+{
+    int i, j, pivot, temp;
+    if (first < last)
+    {
+        pivot = first;
+        i = first;
+        j = last;
+        while (i < j)
+        {
+            while (arr[i] <= arr[pivot] && i < last)
+                i++;
+            while (arr[j] > arr[pivot])
+                j--;
+            if (i < j)
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        temp = arr[pivot];
+        arr[pivot] = arr[j];
+        arr[j] = temp;
+        quicksort(arr, first, j - 1);
+        quicksort(arr, j + 1, last);
+    }
+}
 int main()
 {
     int size;
@@ -77,9 +156,15 @@ int main()
     case 3:
         Insertionsort(array, size);
         break;
-    default:
+    case 4:
+        mergeSort(array, 0, size - 1);
+        break;
+   
+     default:    
+        printf("Invalid choice\n");
         break;
     }
+   
     printf("The sorted array is: ");
     for (int i = 0; i < size; i++)
     {
